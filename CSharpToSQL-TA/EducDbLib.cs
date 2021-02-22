48caacf4dd992b289d43de7f0595adade7fb26b7
Jada <jada.fagan@gmail.com>
2021-02-22 16:32:23 -0500

    Add project files.

---
+++ CSharpToSQL-TA/EducDbLib.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace CSharpToSQL_TA
+{
+    public class EducDbLib
+    {
+        public SqlConnection connection { get; set; }
+
+        public void ExecSelect()
+        {
+            var sql = "SELECT * from Student;";
+            var cmd = new SqlCommand(sql, connection);
+            var reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                var id = Convert.ToInt32(reader["id"]);
+                var lastname = reader["Lastname"].ToString();
+                Console.WriteLine($" Id: {id}, lastname: {lastname}");
+            }
+            reader.Close();
+        }
+
+        public void Connect(string database)
+        {
+            var connStr = $"server=localhost\\sqlexpress;" +
+                           $"database={database};" +
+                           "trusted_connection = true;";
+            connection = new SqlConnection(connStr);
+            connection.Open();
+            if(connection.State != System.Data.ConnectionState.Open)
+            {
+                throw new Exception("Connection not open");
+            }
+        }
+
+        public void Disconnect()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ TestCSharpToSQL-TA/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpToSQL_TA;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace TestCSharpToSQL_TA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sql = new EducDbLib();
+            sql.Connect("EdDb");
+            Console.WriteLine("Connection successful");
+            sql.ExecSelect();
+            sql.Disconnect();
+        }
+    }
+}
